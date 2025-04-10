e769ab50b671c3b45616f1a932dca3f3cb4db9b4
pfjndev <88943477+pfjndev@users.noreply.github.com>
2025-04-10 15:46:50 +0100

    chore: update dependencies and configure TypeScript for Preact
    - Added @astrojs/preact and preact to package.json dependencies.
    - Updated tsconfig.json to include Preact JSX support with appropriate compiler options.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://moonlit-seahorse-aa17d2.netlify.app"
+  site: "https://moonlit-seahorse-aa17d2.netlify.app",
+  integrations: [preact()]
 });
